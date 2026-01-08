0a8b78659b2c45d740f3f4b426f165238c34bd0d
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-09 01:23:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -115,7 +115,7 @@ async def cb_handler(client, query: CallbackQuery):
             ])
         )
     elif data == "file_names":
-        format_template = await N4BOTS.get_format_template(user_id)
+        format_template = await n4bots.get_format_template(user_id)
         await query.message.edit_text(
             text=Txt.FILE_NAME_TXT.format(format_template=format_template),
             disable_web_page_preview=True,
