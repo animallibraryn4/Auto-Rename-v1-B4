d54de9f06e89b29bf46362c310db8b0f24941c77
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-10 01:43:42 +0530

    Update file_router.py

--- plugins/file_router.py
+++ plugins/file_router.py
@@ -2,6 +2,10 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from plugins.admin_panel import check_and_notify_banned_user
+
 class FileRouter:
     def __init__(self):
         self.processing_lock = {}  # user_id -> lock
@@ -54,3 +58,38 @@ file_router = FileRouter()
 async def handle_all_files(client, message):
     """Single entry point for all file processing"""
     await file_router.route_file(client, message)
+
+
+# List of commands that should NOT be blocked for banned users
+ALLOWED_COMMANDS_FOR_BANNED = ["start", "help"]
+
+@Client.on_message(filters.private)
+async def ban_middleware(client: Client, message: Message):
+    """Middleware to check if user is banned before processing any command"""
+    user_id = message.from_user.id
+    
+    # Check if message is a command
+    if message.text and message.text.startswith('/'):
+        # Extract command name (remove the / and any parameters)
+        command_parts = message.text.split()
+        if command_parts:
+            command = command_parts[0][1:].lower()  # Remove / and convert to lowercase
+            
+            # Skip ban check for allowed commands
+            if command in ALLOWED_COMMANDS_FOR_BANNED:
+                return
+            
+            # Check if user is banned and notify them
+            if await check_and_notify_banned_user(client, user_id):
+                # Stop further processing of this command
+                return True  # This will prevent other handlers from processing
+    
+    # For non-command messages (files, text, etc.)
+    else:
+        # Check if user is banned and notify them
+        if await check_and_notify_banned_user(client, user_id):
+            # Stop further processing
+            return True
+    
+    # If not banned or command is allowed, continue processing
+    return False
