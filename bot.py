7d4c6361f07ea77cf571f5f0f4e102d9016b8932
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-09 13:20:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ import pyromod
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import os
 import time
-import plugins.quality_thumb  # Explicitly import the module
 
 pyrogram.utils.MIN_CHANNEL_ID = -1001896877147
 
