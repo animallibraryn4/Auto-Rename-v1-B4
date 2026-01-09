ad2a65c09b392eeb1c7f46bc8c1f8e950611f085
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-10 02:52:15 +0530

    Add files via upload

---
+++ plugins/state_manager.py
@@ -0,0 +1,19 @@
+# 
+class UserState:
+    def __init__(self):
+        self.info_mode = {}
+        self.sequence_mode = {}
+        self.verification = {}
+        self.queues = {}
+    
+    async def get_user_state(self, user_id):
+        """Return current user operation mode"""
+        if user_id in self.info_mode:
+            return "info"
+        elif user_id in self.sequence_mode:
+            return "sequence"
+        else:
+            return "auto_rename"
+
+# Global state manager
+user_state_manager = UserState()
