4fac034255d1995798feff7b49e40e72b6671c3b
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-12 01:38:26 +0530

    Add files via upload

---
+++ plugins/caption.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters 
+from helper.database import n4bots
+
+@Client.on_message(filters.private & filters.command('set_caption'))
+async def add_caption(client, message):
+    if len(message.command) == 1:
+       return await message.reply_text("**Give The Caption\n\nExample :- `/set_caption 📕Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
+    caption = message.text.split(" ", 1)[1]
+    await n4bots.set_caption(message.from_user.id, caption=caption)
+    await message.reply_text("**Your Caption Successfully Added ✅**")
+
+@Client.on_message(filters.private & filters.command('del_caption'))
+async def delete_caption(client, message):
+    caption = await n4bots.get_caption(message.from_user.id)  
+    if not caption:
+       return await message.reply_text("**You Don't Have Any Caption ❌**")
+    await n4bots.set_caption(message.from_user.id, caption=None)
+    await message.reply_text("**Your Caption Successfully Deleted 🗑️**")
+
+@Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
+async def see_caption(client, message):
+    caption = await n4bots.get_caption(message.from_user.id)  
+    if caption:
+       await message.reply_text(f"**Your Caption :**\n\n`{caption}`")
+    else:
+       await message.reply_text("**You Don't Have Any Caption ❌**")
+
+
